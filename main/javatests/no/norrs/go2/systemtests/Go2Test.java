524acac11f35ba70bfe3b67c4f9cf24a8900651e
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2016-01-08 11:25:11 +0100

    System test for view redirect link option provided.

--- main/javatests/no/norrs/go2/systemtests/Go2Test.java
+++ main/javatests/no/norrs/go2/systemtests/Go2Test.java
@@ -61,4 +61,40 @@ public class Go2Test extends AbstractHttpClientServerTest {
         Assert.assertEquals(HttpServletResponse.SC_FOUND, response2.getStatus());
         Assert.assertEquals(redirectTarget, response2.getHeaders().get("Location"));
     }
+
+    @Test
+    public void test_RegisterAndVisitShortName_Option_ViewRedirectLink() throws Exception {
+        ServletHandler handler = new ServletHandler();
+        handler.addServletWithMapping(Register.class, "/register");
+        handler.addServletWithMapping(Go2.class, "/*");
+        start(handler);
+
+        String shortName = "testRegisterAndVisitShortNameViewRedirectLink" + new Date().getTime();
+        String redirectTarget = "http://github.com/norrs/go2";
+
+        Request request = client.POST(scheme + "://localhost:" + connector.getLocalPort() + "/register");
+
+        request.param("shortName", shortName);
+        request.param("redirectTarget", redirectTarget);
+        Response response =  request.send();
+
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(HttpServletResponse.SC_CREATED, response.getStatus());
+
+        Request request2 = client.newRequest(scheme  + "://localhost:" + connector.getLocalPort() + "/"  + shortName + "/view");
+        request2.followRedirects(false);
+        Response response2 = request2.send();
+
+        Request request3 = client.newRequest(scheme  + "://localhost:" + connector.getLocalPort() + "/"  + shortName + "/v");
+        request3.followRedirects(false);
+        Response response3 = request3.send();
+
+
+        Assert.assertEquals(HttpServletResponse.SC_FOUND, response2.getStatus());
+        Assert.assertFalse(response2.getHeaders().containsKey("Location"));
+
+        Assert.assertEquals(HttpServletResponse.SC_FOUND, response3.getStatus());
+        Assert.assertFalse(response3.getHeaders().containsKey("Location"));
+    }
 }
